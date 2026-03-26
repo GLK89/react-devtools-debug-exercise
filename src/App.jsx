03f1721b4007ae88b3c9bc9b901db2d2d3139cd4
Ginger Korporaal <gingerkorporaal@hotmail.com>
2026-03-26 11:13:01 +0100

    fix: debugged app and resolved issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import { Background } from './components/Background'
+import { useState } from "react";
+import "./App.css";
+import { Background } from "./components/Background";
 
 function App() {
-    const [color, setColor] = useState('red');
+  const [color, setColor] = useState("red");
 
-    const setColorRed = () => {
-        setColor('red');
-    }
+  const setColorRed = () => {
+    setColor("red");
+  };
 
-    const setColorGreen = () => {
-        setColor('green');
-    }
+  const setColorGreen = () => {
+    setColor("green");
+  };
 
-    const setColorBlue = () => {
-        setColor();
-    }
+  const setColorBlue = () => {
+    setColor("blue");
+  };
 
-    return (
-        <>
-            <Background color={color} />
-            <h1>Color Picker</h1>
+  return (
+    <>
+      <Background color={color} />
+      <h1>Color Picker</h1>
 
-            <button onClick={setColorRed}>
-                Red
-            </button>
-            <button onClick={setColorGreen}>
-                Green
-            </button>
-            <button onClick={setColorBlue}>
-                Blue
-            </button>
-        </>
-    )
+      <button onClick={setColorRed}>Red</button>
+      <button onClick={setColorGreen}>Green</button>
+      <button onClick={setColorBlue}>Blue</button>
+    </>
+  );
 }
 
-export default App
+export default App;
